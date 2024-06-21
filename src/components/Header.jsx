fbbde2bdec66f402d9d938599f9be721ad833c3c
Abhishek Kumar <abhi28535@gmail.com>
2024-06-21 20:46:04 +0530

    footer completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import { Button, Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
+import { Button,Nav, Navbar, } from 'react-bootstrap';
 import Group from '/Group.jpg';
 import "./Header.css";
 
 const Header = () => {
   return (
-    <Navbar className='navbar'>        
-            <div>
+    <Navbar className="navbar col-md-12 justify-content-between px-2">
+      <div>
         <img src={Group} alt="LogoImage" />
+      </div>
+
+      <Nav className="nav-options">
+        <Nav.Link href="#">About</Nav.Link>
+        <Nav.Link href="#">Facilites</Nav.Link>
+        <Nav.Link href="#">Training</Nav.Link>
+        <Nav.Link href="#">Events</Nav.Link>
+        <Nav.Link href="#">Team</Nav.Link>
+        <Nav.Link href="#">Connect</Nav.Link>
+        <Nav.Link href="#">Resources</Nav.Link>
+        <div>
+          <Button className="button-involved">Get Inovlved</Button>
         </div>
-        
-        <Nav className="nav-options">
-          <Nav.Link href="#">About</Nav.Link>
-          <Nav.Link href="#">Facilites</Nav.Link>
-          <Nav.Link href="#">Training</Nav.Link>
-          <Nav.Link href="#">Events</Nav.Link>
-          <Nav.Link href="#">Team</Nav.Link>
-          <Nav.Link href="#">Connect</Nav.Link>
-          <Nav.Link href="#">Resources</Nav.Link>
-          <div>
-          <Button className='btn-involved'>Get Inovlved</Button></div>
-        </Nav>
-       
+      </Nav>
     </Navbar>
   );
 }
