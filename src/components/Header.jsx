4a1105168a37ae4b0c51df773cceb41573a6c1b2
Abhishek Kumar <abhi28535@gmail.com>
2024-06-23 19:15:34 +0530

    fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import { Button,Nav, Navbar, } from 'react-bootstrap';
+import React from 'react';
+import { Button, Nav, Navbar } from 'react-bootstrap';
 import Group from '/Group.jpg';
-import "./Header.css";
+import './Header.css';
 
 const Header = () => {
   return (
-    <Navbar className="navbar justify-content-between px-2">
-      <div>
+    <Navbar expand="lg" className="navbar px-2">
+      <Navbar.Brand href="#">
         <img src={Group} alt="LogoImage" />
-      </div>
-
-      <Nav className="nav-options">
-        <Nav.Link href="#">About</Nav.Link>
-        <Nav.Link href="#">Facilites</Nav.Link>
-        <Nav.Link href="#">Training</Nav.Link>
-        <Nav.Link href="#">Events</Nav.Link>
-        <Nav.Link href="#">Team</Nav.Link>
-        <Nav.Link href="#">Connect</Nav.Link>
-        <Nav.Link href="#">Resources</Nav.Link>
-        <div>
-          <Button className="btn-involved">Get Inovlved</Button>
-        </div>
-      </Nav>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="nav-options ms-auto">
+          <Nav.Link href="#">About</Nav.Link>
+          <Nav.Link href="#">Facilities</Nav.Link>
+          <Nav.Link href="#">Training</Nav.Link>
+          <Nav.Link href="#">Events</Nav.Link>
+          <Nav.Link href="#">Team</Nav.Link>
+          <Nav.Link href="#">Connect</Nav.Link>
+          <Nav.Link href="#">Resources</Nav.Link>
+          <div className="nav-button">
+            <Button className="btn-involved">Get Involved</Button>
+          </div>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
 
-export default Header
+export default Header;
